89a14481aa1ba223961a080b2cefcd21a724c783
zero <silver@silver.silver>
2025-03-22 13:31:02 +0100

    db to port 5433

--- backend/app/db-config.js
+++ backend/app/db-config.js
@@ -3,7 +3,7 @@ module.exports = {
   host: "db",
   database: "root",
   password: "root",
-  port: 5432, // Default PostgreSQL port
+  port: 5433, // Default PostgreSQL port
   max: 10, // Maximum number of connections in the pool
   idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
 };
